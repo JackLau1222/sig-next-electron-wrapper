73b6e431071344f5682a255b7114dc96f667c10a
ziggy615 <ziggy16603042386@163.com>
2024-10-29 01:29:46 +0800

    refactor: Rename some envs

--- electron-wrapper-next/recipes/org.deepin.games-demo-1.1.0/inject.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    'use strict';
-
-    const origin = HTMLElement.prototype.requestPointerLock
-    HTMLElement.prototype.requestPointerLock = function () {
-        return origin.call(this)
-    }
-})();
